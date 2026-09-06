cc12124ea1b5ba8862077a3913dd20bf1d6f2d2e
Оля Михеева <olamiheeva@MacBook-Air-Ola.local>
2026-09-06 12:04:03 +0300

    Extract test data and generation in application modules

---
+++ data.py
@@ -0,0 +1,3 @@
+TEST_EMAIL = "olya_mikheeva_53_001@yandex.ru"
+TEST_PASSWORD = "123456789"
+BASE_URL = "https://stellarburgers.education-services.ru/"
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-import random
 from locators import (
     BUTTON_PERSONAL_ACCOUNT,
     BUTTON_REGISTRATION_ON_PERSONAL_ACCOUNT,
@@ -11,21 +10,25 @@ from locators import (
     BUTTON_LOGIN,
     PASSWORD_ERROR
 )
+from data import BASE_URL
+from utils import generate_email, generate_password
+
+
 
 class TestRegistration:
 
     def test_successful_registration(self, driver):
-        driver.get("https://stellarburgers.education-services.ru/")
+        driver.get(BASE_URL)
         
         WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(BUTTON_PERSONAL_ACCOUNT)).click()
         
         driver.find_element(*BUTTON_REGISTRATION_ON_PERSONAL_ACCOUNT).click()
         driver.find_element(*NAME).send_keys("Оля")
         
-        email = f"{random.randint(100000, 999999)}@yandex.ru"
+        email = generate_email()
         driver.find_element(*EMAIL).send_keys(email)
-        
-        password = f"{random.randint(100000, 999999)}"
+
+        password = generate_password()
         driver.find_element(*PASSWORD).send_keys(password)
         
         driver.find_element(*BUTTON_REGISTRATION).click()
@@ -37,17 +40,17 @@ class TestRegistration:
 
         
     def test_registration_with_incorrect_password(self, driver):
-        driver.get("https://stellarburgers.education-services.ru/")
+        driver.get(BASE_URL)
     
         WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(BUTTON_PERSONAL_ACCOUNT)).click()
     
         driver.find_element(*BUTTON_REGISTRATION_ON_PERSONAL_ACCOUNT).click()
         driver.find_element(*NAME).send_keys("Оля")
     
-        email = f"{random.randint(100000, 999999)}@yandex.ru"
+        email = generate_email()
         driver.find_element(*EMAIL).send_keys(email)
-    
-        password = f"{random.randint(10000, 99999)}"
+        
+        password = "12345"
         driver.find_element(*PASSWORD).send_keys(password)
     
         driver.find_element(*BUTTON_REGISTRATION).click()
